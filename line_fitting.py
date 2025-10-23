726dcd6b118c0836ad82ce5734f43a239bff71c8
sameeyaa <sameeya005@gmail.com>
2025-10-23 11:03:47 +0100

    fixing errors in the code

--- line_fitting.py
+++ line_fitting.py
@@ -3,15 +3,15 @@ from scipy import stats
 import matplotlib.pyplot as plt
 import numpy as np
 
-months = np.array([5,10,15,20,25,30,35])
-sales = np.array([50,100,150,200,250,300])
+months = np.array([5,13,15,20,25,30,32,40,45,50,58,60])
+sales = np.array([50,100,147,200,250,305,350,420,450,500,550,600])
 
-slope,intercept,r_value,p_value, std_err = stats.linegress(months, sales)
+slope,intercept,r_value,p_value, std_err = stats.linregress(months, sales)
 
-best_fit_line = slope*months + intercept
+line_of_best_fit = slope*months + intercept
 
 plt.scatter(months, sales, color = 'blue', label = 'Sales')
-plt.plot(months, best_fit_line_color = 'pink', label = 'Line of Best fit')
+plt.plot(months,line_of_best_fit, color = 'pink', label = 'Line of Best fit')
 plt.xlabel('Month')
 plt.ylabel('Sales')
 plt.title('Monthly Sales with Regression')

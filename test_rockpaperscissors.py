118d91ffcfb52090e65e6407bd527bda800ab5a0
sameeyaa <sameeya005@gmail.com>
2025-11-14 12:14:44 +0000

    check if player and computer can draw

--- test_rockpaperscissors.py
+++ test_rockpaperscissors.py
@@ -10,3 +10,8 @@ def test_computer_wins():
     assert get_winner("scissors", "rock") == "computer"
     assert get_winner("paper", "scissors") == "computer"
 
+def test_draw():
+    assert get_winner("rock", "rock") == "draw"
+    assert get_winner("paper", "paper") == "draw"
+    assert get_winner("scissors", "scissors") == "draw"
+

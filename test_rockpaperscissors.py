ced2158f10842b9f74d5449c322baf0e2cb28872
sameeyaa <sameeya005@gmail.com>
2025-11-14 12:11:33 +0000

    check if computer is able to win

--- test_rockpaperscissors.py
+++ test_rockpaperscissors.py
@@ -5,3 +5,8 @@ def test_player_wins():
     assert get_winner("scissors", "paper") == "player"
     assert get_winner("paper", "rock") == "player"
 
+def test_computer_wins():
+    assert get_winner("rock", "paper") == "computer"
+    assert get_winner("scissors", "rock") == "computer"
+    assert get_winner("paper", "scissors") == "computer"
+

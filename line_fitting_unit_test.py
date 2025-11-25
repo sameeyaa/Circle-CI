500309b81ca71c161853d7bf8cd2c881c030e1dd
sameeyaa <sameeya005@gmail.com>
2025-11-25 19:06:24 +0000

    setting unit test values

--- line_fitting_unit_test.py
+++ line_fitting_unit_test.py
@@ -1,2 +1,11 @@
 #line fitting unit test
 
+import numpy as np
+from line_fitting import make_regression, make_best_fit_line
+
+def test_regression_values():
+    months = np.array([5,13,15,20,25,30,32,40,45,50,58,60])
+    sales = np.array([50,100,147,200,250,305,350,420,450,500,550,600])
+
+    slope, intercept, r_value, p_value, std_err = make_regression(months, sales)
+

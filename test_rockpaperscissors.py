2c26ed3b61943b89c0ca43e910b6f5163b0a5507
sameeyaa <sameeya005@gmail.com>
2025-11-25 19:28:40 +0000

    test if player wins

--- test_rockpaperscissors.py
+++ test_rockpaperscissors.py
@@ -1,20 +1,8 @@
-from rockpaperscissors import get_winner
+import pytest
+from rockpaperscissors import get_winner, get_computer_option, options
 
 def test_player_wins():
     assert get_winner("rock", "scissors") == "player"
-    assert get_winner("scissors", "paper") == "player"
     assert get_winner("paper", "rock") == "player"
-
-def test_computer_wins():
-    assert get_winner("rock", "paper") == "computer"
-    assert get_winner("scissors", "rock") == "computer"
-    assert get_winner("paper", "scissors") == "computer"
-
-def test_draw():
-    assert get_winner("rock", "rock") == "draw"
-    assert get_winner("paper", "paper") == "draw"
-    assert get_winner("scissors", "scissors") == "draw"
-
-
-
+    assert get_winner("scissors", "paper") == "player"
 

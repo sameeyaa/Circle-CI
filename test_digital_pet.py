3f2dd4b8dd57be0ae076c430f9c15b2c68a98605
sameeyaa <sameeya005@gmail.com>
2025-11-29 11:50:38 +0000

    testing sleep and wake function

--- test_digital_pet.py
+++ test_digital_pet.py
@@ -22,5 +22,19 @@ class TestDigitalPet(unittest.TestCase):
         self.assertEqual(pet.hunger, 8)
         self.assertEqual(pet.happiness, 7)
         self.assertIn("fed", pet.interactions)
-         
+
+    #test sleeping function
+    def test_sleep(self):
+        pet = DigitalPet("MyPet")
+        pet.sleep()
+        self.assertTrue(pet.sleeping)
+        self.assertEqual(pet.energy, 8)
+
+    def test_wake_up(self):
+        pet = DigitalPet("MyPet")
+        pet.sleeping = Truepet.energy = 3
+        pet.wake_up()
+        self.assertEqual(oet.energy, 15) #test if vitals exceed the cap of 10
+
+       
 
